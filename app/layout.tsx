cb790864a654904c4855ed39d2ef6df9657f3d59
Abdou Mohi <couponwolf01@gmail.com>
2026-04-22 02:46:53 +0100

    add monetag verification meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased">
+      
+      {/* ✅ MONETAG VERIFICATION */}
+      <head>
+        <meta name="monetag" content="2c316c201ef8888ddab6cf8ba3a40d49" />
+      </head>
+
       <body className={`${inter.className} min-h-full flex flex-col`}>
         {children}
       </body>
